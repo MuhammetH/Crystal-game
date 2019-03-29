0b68edf95f95c6e80a4115a029ce7f3a4ca9d970
MuhammetD <doktor.oglu333@gmail.com>
2019-03-28 22:09:54 -0500

    added bunch of things keep forgetting to commit yee haawww

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,26 @@ var randomNumber;
 var lost;
 var win;
 
-for(var i = 0; i < 4;i++){
-    var random = Math.floor(Math.random() * 12);
+//$(".crystal").attr('class', 'red');  setting value to crystal
+
+randomNumber = Math.floor((Math.random()* 101) + 19);// generates a number between  19-120
+$("#result").html('Random Results: ');
+console.log(randomNumber)
+
+
+for(var i = 0; i < 4;i++){// generates random number between 12-1 for each crystal 
+    var random = Math.floor((Math.random() * 11)+ 1);
     console.log(random)
 
     var crystal = $("<div>");
         crystal.attr({
          "class": 'crystal',
-         "data-random": random
+         "data-random": random// shows the random number for each crystal at sources 
      });
 
     $(".crystals").append(crystal);
 }
+
+$(".crystal").on('click', function(){
+    console.log($(this).attr('data-random'));
+});
